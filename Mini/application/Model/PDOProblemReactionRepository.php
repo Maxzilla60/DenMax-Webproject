9476924c6602c4826b37ca43a39a7a47591fdc15
Unknown <dennisheperol@gmail.com>
2017-10-10 17:02:45 +0200

    Implemented problemReactions + PDO

---
+++ Mini/application/Model/PDOProblemReactionRepository.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dennis
+ * Date: 10.10.17
+ * Time: 16:51
+ */
+
+namespace Mini\Model;
+
+use Mini\Core\Model;
+
+
+class PDOProblemReactionRepository extends Model
+{
+    public function getAllProblemReactions()
+    {
+        $sql = "SELECT * FROM problemreactions";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        $fetchedProblemReactions = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $problemReactionArray = array();
+        if (count($fetchedProblemReactions) > 0) {
+            foreach ($fetchedProblemReactions as $pr) {
+                $problemReactionArray[] = new ProblemReaction($pr['id'], $pr['problem_id'], $pr['rating'], $pr['description']);
+            }
+        }
+
+        return $problemReactionArray;
+    }
+
+    public function getProblemReactionsByProblemId($problem_id)
+    {
+        $sql = "SELECT * FROM problemreactions WHERE problem_id = :problem_id";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':problem_id' => $problem_id);
+        $query->execute($parameters);
+        $fetchedProblemReactions = $query->fetchAll();
+
+        $problemReactionArray = array();
+        if (count($fetchedProblemReactions) > 0) {
+            foreach ($fetchedProblemReactions as $pr) {
+                $problemReactionArray[] = new ProblemReaction($pr['id'], $pr['problem_id'], $pr['rating'], $pr['description']);
+            }
+        }
+
+        return $problemReactionArray;
+    }
+}
