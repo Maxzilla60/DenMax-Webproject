ba10384f0b4f72308001a3fa65c74be1066d936a
Maxime Orione <maxpower98@hotmail.com>
2017-10-10 15:59:19 +0200

    Implemented Company & Employee classes

---
+++ Mini/application/Model/Company.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mini\Model;
+
+class Company
+{
+    private $id;
+    private $name;
+
+    public function __construct($id, $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function toJSON() {
+        // TODO
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
