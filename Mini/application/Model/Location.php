317351e7119444c65719b8cba65a465a8cb4a507
Maxime Orione <maxpower98@hotmail.com>
2017-10-10 15:14:54 +0200

    Mini Dump

---
+++ Mini/application/Model/Location.php
@@ -0,0 +1,49 @@
+<?php
+
+class Location
+{
+    private $id;
+    private $name;
+    private $company_id;
+
+    public function __construct($id, $name, $company_id)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->company_id = $company_id;
+    }
+
+    public function toJSON() {
+        //TODO
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getCompanyId()
+    {
+        return $this->company_id;
+    }
+
+    public function setCompanyId($company_id)
+    {
+        $this->company_id = $company_id;
+    }
+}
---
+++ Mini/application/Model/PDOLocationRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Mini\Model;
+
+use Mini\Core\Model;
+use Location;
+use StatusReport;
+
+class PDOLocationRepository extends Model
+{
+    public function getAllLocations()
+    {
+        $sql = "SELECT * FROM locations";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        $fetchedLocations = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $locationsArray = array();
+        if (count($fetchedLocations) > 0) {
+            foreach ($fetchedLocations as $l) {
+                $locationsArray[] = new Location($l['id'], $l['name'], $l['company_id']);
+            }
+        }
+
+        return $locationsArray;
+    }
+
+    public function getStatusReport($location_id)
+    {
+        $sql = "SELECT status, date FROM statusreports WHERE location_id = :location_id";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':location_id' => $location_id);
+        $query->execute($parameters);
+        $fetchedReports = $query->fetchAll();
+
+        $reportsArray = array();
+        if (count($fetchedReports) > 0) {
+            foreach ($fetchedReports as $r) {
+                $reportsArray[] = new StatusReport($r['id'], $r['location_id'], $r['status'], $r['date']);
+            }
+        }
+
+        return $reportsArray;
+    }
+    
+    public function get($status_id)
+    {
+    }
+}
