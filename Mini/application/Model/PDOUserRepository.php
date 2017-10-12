575f7536c77e593a87bc471f0dd680de3eaa4f6a
Unknown <dennisheperol@gmail.com>
2017-10-12 11:34:05 +0200

    Added user

---
+++ Mini/application/Model/PDOUserRepository.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dennis
+ * Date: 10.10.17
+ * Time: 16:51
+ */
+
+namespace Mini\Model;
+
+use Mini\Core\Model;
+
+
+class PDOUserRepository extends Model
+{
+    public function getAllUsers()
+    {
+        $sql = "SELECT * FROM users";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        $fetchedUsers = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $userArray = array();
+        if (count($fetchedUsers) > 0) {
+            foreach ($fetchedUsers as $u) {
+                $problemReactionArray[] = new User($u['id'], $u['name'], $u['role']);
+            }
+        }
+
+        return $userArray;
+    }
+
+    public function geUsersByRole($role)
+    {
+        $sql = "SELECT * FROM users WHERE role = :role";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':role' => $role);
+        $query->execute($parameters);
+        $fetchedUsers = $query->fetchAll();
+
+        $userArray = array();
+        if (count($fetchedUsers) > 0) {
+            foreach ($fetchedUsers as $u) {
+                $UserArray[] = new User($u['id'], $u['name'], $u['role']);
+            }
+        }
+
+        return $userArray;
+    }
+}
